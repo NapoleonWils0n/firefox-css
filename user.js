6743772cf3dfbb33d3f47ba37fe98e0002318bad
Daniel J Wilcox <danieljwilcox@gmail.com>
2021-08-11 17:14:37 +0100

    firefox privacy tweaks and youtube css

--- user.js
+++ user.js
@@ -5,6 +5,21 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // disable firefox sync
 user_pref("identity.fxaccounts.enabled", false);
 
+// disable geo location
+user_pref("geo.enabled", false);
+
+// disable referer
+user_pref("network.http.sendRefererHeader", false);
+
+// disable network prefetch
+user_pref("network.prefetch-next", false);
+
+// privacy.firstparty.isolate
+user_pref("privacy.firstparty.isolate", true);
+
+// privacy.resistFingerprinting
+user_pref("privacy.resistFingerprinting", true);
+
 // Fill SVG Color
 user_pref("svg.context-properties.content.enabled", true);
 
