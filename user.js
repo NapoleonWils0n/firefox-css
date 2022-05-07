eca2cdb52691624a2a4ef788305de6fcdca18404
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-05-07 19:04:40 +0100

    firefox 100

--- user.js
+++ user.js
@@ -2,29 +2,9 @@
 // userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// disable firefox sync
-user_pref("identity.fxaccounts.enabled", false);
+// disable pocket
+user_pref("extensions.pocket.enabled", false);
 
-// disable geo location
-user_pref("geo.enabled", false);
+// dont display download pop up
+user_pref("browser.download.alwaysOpenPanel", false);
 
-// disable referer
-user_pref("network.http.sendRefererHeader", false);
-
-// disable network prefetch
-user_pref("network.prefetch-next", false);
-
-// privacy.firstparty.isolate
-user_pref("privacy.firstparty.isolate", true);
-
-// privacy.resistFingerprinting
-user_pref("privacy.resistFingerprinting", true);
-
-// Fill SVG Color
-user_pref("svg.context-properties.content.enabled", true);
-
-// CSS Blur Filter - 88 Above
-user_pref("layout.css.backdrop-filter.enabled", true);
-
-// Restore Compact Mode - 89 Above
-user_pref("browser.compactmode.show", true);
