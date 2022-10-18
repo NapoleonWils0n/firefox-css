a35af140ab15334b2f7a92d3a0f9a40276f6bd6d
Daniel J Wilcox <danieljwilcox@gmail.com>
2022-10-18 20:09:09 +0100

    firefox 106 - hide tabmanager

--- user.js
+++ user.js
@@ -8,3 +8,5 @@ user_pref("extensions.pocket.enabled", false);
 // dont display download pop up
 user_pref("browser.download.alwaysOpenPanel", false);
 
+// firefox 106 - hide tabmanager
+user_pref("browser.tabs.tabmanager.enabled", false);
